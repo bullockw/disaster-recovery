9562a6c8e6492ae8414bca7d151d76cab0ab9ea5
Will Bullock <bullock.gwill@gmail.com>
2020-09-28 11:15:36 -0500

    new terraform

--- terraform/DR-ORDS-RW/data_sources.tf
+++ terraform/DR-ORDS-RW/data_sources.tf
@@ -6,13 +6,6 @@ data "oci_identity_tenancy" "tenancy" {
   tenancy_id = var.tenancy_ocid
 }
 
-data "oci_identity_regions" "home-region" {
-  filter {
-    name   = "key"
-    values = [data.oci_identity_tenancy.tenancy.home_region_key]
-  }
-}
-
 # Availability Domains for Primary Region
 data oci_identity_availability_domains ADs {
   compartment_id = var.tenancy_ocid
--- terraform/DR-ORDS-RW/providers.tf
+++ terraform/DR-ORDS-RW/providers.tf
@@ -2,14 +2,6 @@
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 ### Primary Region provider
-provider "oci" {
-  alias        = "home"
-  region       = lookup(data.oci_identity_regions.home-region.regions[0], "name")
-  tenancy_ocid = var.tenancy_ocid
-
-}
-
-### Primary Region provider
 provider oci {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
